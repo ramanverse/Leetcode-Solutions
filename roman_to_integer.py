97a253f86532dacca007d363a22434babd412e98
ramanverse <associatedbull@gmail.com>
2026-02-13 10:50:00 +0530

    Roman to Integer - Handling Subtractive Cases

---
+++ roman_to_integer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def romanToInt(self, s):
+        d = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+        
+        ans = 0
+        
+        for i in range(len(s)):
+            if i > 0 and d[s[i]] > d[s[i - 1]]:
+                ans -= 2 * d[s[i - 1]]
+            ans += d[s[i]]
+        
+        return ans

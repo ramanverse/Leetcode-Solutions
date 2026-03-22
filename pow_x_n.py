da7219ef9725eda8f9aed07aae401a7773a07d27
ramanverse <associatedbull@gmail.com>
2026-03-22 11:20:00 +0530

    Pow(x, n) - Fast Exponentiation using Recursion

---
+++ pow_x_n.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n == 0:
+            return 1
+        
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        
+        if n & 1:
+            return x * self.myPow(x * x, n // 2)
+        
+        return self.myPow(x * x, n // 2)

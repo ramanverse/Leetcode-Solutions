a9eccbcf93a1fb6973b552f417d8a648f264dc44
ramanverse <associatedbull@gmail.com>
2026-03-28 11:25:00 +0530

    Merge Intervals - Greedy Sorting Approach

---
+++ merge_intervals.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        
+        ans = []
+        
+        for i in intervals:
+            if ans and i[0] <= ans[-1][1]:
+                ans[-1][1] = max(ans[-1][1], i[1])
+            else:
+                ans.append(i)
+        
+        return ans
